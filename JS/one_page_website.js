4a222e6898f70a9a8af7fd082ffb5ed8e3b654b3
CostinaD <costinadogaru@gmail.com>
2024-07-04 19:51:58 +0100

    Updates on the website

---
+++ JS/one_page_website.js
@@ -0,0 +1,54 @@
+// JS/one_page_website.js
+
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.gallery-container img');
+const fullSizeImages = [
+    "Images/Fullsize/Casting1.png",
+    "Images/Fullsize/Casting2.png",
+    "Images/Fullsize/Casting3.png",
+    "Images/Fullsize/Casting4.png",
+    "Images/Fullsize/Casting5.png",
+    "Images/Fullsize/Casting6.png",
+    "Images/Fullsize/Casting7.png",
+    "Images/Fullsize/Casting8.png",
+    "Images/Fullsize/Casting9.png",
+    "Images/Fullsize/Casting10.png"
+];
+
+function openLightbox(index) {
+    currentImageIndex = index;
+    document.getElementById("lightbox-img").src = fullSizeImages[currentImageIndex];
+    document.getElementById("lightbox-container").style.display = "block";
+    loadThumbnails();
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox-container").style.display = "none";
+}
+
+function navigateLightbox(direction) {
+    currentImageIndex += direction;
+    if (currentImageIndex < 0) {
+        currentImageIndex = fullSizeImages.length - 1;
+    } else if (currentImageIndex >= fullSizeImages.length) {
+        currentImageIndex = 0;
+    }
+    document.getElementById("lightbox-img").src = fullSizeImages[currentImageIndex];
+}
+
+function loadThumbnails() {
+    const thumbnailContainer = document.getElementById("lightbox-thumbnails");
+    thumbnailContainer.innerHTML = "";
+    fullSizeImages.forEach((src, index) => {
+        const thumb = document.createElement("img");
+        thumb.src = src.replace("Fullsize", "Thumbnails");
+        thumb.onclick = () => openLightbox(index);
+        thumbnailContainer.appendChild(thumb);
+    });
+}
+
+document.getElementById("lightbox-container").addEventListener('click', function(event) {
+    if (event.target === this) {
+        closeLightbox();
+    }
+});
